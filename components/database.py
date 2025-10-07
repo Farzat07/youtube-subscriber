0d0b241c3b437736d80b06966c95db1a7cc5db2e
A Farzat <a@farzat.xyz>
2025-10-07 11:04:40 +0300

    Secure mongodb and rabbitmq containers

--- components/database.py
+++ components/database.py
@@ -1,12 +1,19 @@
 import atexit
+from os import getenv
 from typing import Any, Dict
+from dotenv import load_dotenv
 from pymongo import MongoClient
 from pymongo.database import Database
 from pymongo.collection import Collection
 from components.subscriptions.typing import SubsDict
 from components.users.typing import UserDict
 
-client: MongoClient[Any] = MongoClient("mongodb://localhost", tz_aware=True)
+load_dotenv('.env')
+
+client: MongoClient[Any] = MongoClient(
+    "mongodb://%s:%s@localhost/admin" % (getenv('MONGO_USER'), getenv('MONGO_PASS')),
+    tz_aware=True,
+)
 database: Database[Any] = client.get_database("youtube")
 subscriptions: Collection[SubsDict] = database.get_collection("subscriptions")
 users: Collection[UserDict] = database.get_collection("users")
